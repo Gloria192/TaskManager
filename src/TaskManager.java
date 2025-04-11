5bb20e10f19f76e9487a7c46de39ac95c5b4a0f8
Gloria <mutuyegloria@gmail.com>
2025-04-11 03:24:44 -0700

    the second commit

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -3,7 +3,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-// Custom exception for invalid tasks
+
 class InvalidTaskException extends IllegalArgumentException {
     public InvalidTaskException(String message) {
         super(message);
